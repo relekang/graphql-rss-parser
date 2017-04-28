b925adcfb900944f79b46aa319ddfc2277e7b3ff
Rolf Erik Lekang <me@rolflekang.com>
2017-04-28 20:14:43 +0200

    feat: Add error reporting with sentry

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const release = require('./package.json').version;
+
+if (process.env.RAVEN_DSN) {
+  var Raven = require('raven');
+  Raven.config(process.env.RAVEN_DSN, {release}).install();
+}
+
 const url = require('url');
 const { sendError } = require('micro');
 const { parse } = require('./parser');
+
 
 const exampleLink = (host) => `http://${host}/?feed=https://rolflekang.com/feed.xml`;
 
@@ -10,9 +18,15 @@ module.exports = async (req, res) => {
     return { usage: exampleLink(req.headers.host) };
   }
 
+
   try {
     return await parse(query.feed);
   } catch (error) {
     sendError(req, res, error);
+    if (process.env.RAVEN_DSN) {
+      Raven.captureException(error, {
+        extra: {query}
+      })
+    }
   }
 };
