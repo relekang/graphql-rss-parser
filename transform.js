4538086275449d06af963df26b31491c7927c2ea
Rolf Erik Lekang <me@rolflekang.com>
2017-05-02 16:59:15 +0200

    fix: Remove unecessary logging statements

--- transform.js
+++ transform.js
@@ -2,7 +2,6 @@ const map = require('lodash-fp').map
 const isUrl = require('is-url')
 
 function transformEntry(entry) {
-  console.log(!entry.link, isUrl(entry.title))
   return Object.assign({}, entry, {
     link: !entry.link && isUrl(entry.title) ? entry.title : entry.link
   })
@@ -11,7 +10,6 @@ function transformEntry(entry) {
 const transformEntries = map(transformEntry)
 
 module.exports = function transform(feed) {
-  console.log(feed)
   return Object.assign({}, feed, {
     entries: transformEntries(feed.entries)
   })
