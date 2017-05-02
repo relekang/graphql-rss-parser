c4ca2a146f8dd81b6e3f488ff0a202ce570c05dc
Rolf Erik Lekang <me@rolflekang.com>
2017-05-02 17:30:34 +0200

    fix: Add default values in transform

--- transform.js
+++ transform.js
@@ -1,8 +1,20 @@
 const map = require('lodash-fp').map
 const isUrl = require('is-url')
 
+const feedDefaults = {
+  title: null,
+  link: null,
+  pubDate: null,
+}
+
+const itemDefaults = {
+  title: null,
+  link: null,
+  pubDate: null,
+}
+
 function transformEntry(entry) {
-  return Object.assign({}, entry, {
+  return Object.assign({}, feedDefaults, entry, {
     link: !entry.link && isUrl(entry.title) ? entry.title : entry.link
   })
 }
@@ -10,7 +22,7 @@ function transformEntry(entry) {
 const transformEntries = map(transformEntry)
 
 module.exports = function transform(feed) {
-  return Object.assign({}, feed, {
+  return Object.assign({}, itemDefaults, feed, {
     entries: transformEntries(feed.entries)
   })
 }
