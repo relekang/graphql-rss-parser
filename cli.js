f70f7f35bca7ebf4fb5734d8375b8ae507e58662
Rolf Erik Lekang <me@rolflekang.com>
2017-05-02 22:21:02 +0200

    feat: Add cli script

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const updateNotifier = require('update-notifier')
+const args = require('args')
+const micro = require('micro')
+
+const pkg = require('./package.json')
+const createHandler = require('./index')
+
+updateNotifier({ pkg }).notify()
+
+const options = args
+      .option(['p', 'port'], 'Port to listen on', process.env.PORT || 3000, Number)
+      .option(['H', 'host'], 'Host to listen on', process.env.HOST || '0.0.0.0')
+      .option(['R', 'raven-dsn'], 'Raven DSN', process.env.RAVEN_DSN)
+      .parse(process.argv, { name: 'micro-rss-parser' })
+
+console.log(options)
+
+const server = micro(createHandler(options))
+
+server.listen(3000)
--- index.js
+++ index.js
@@ -1,61 +1,63 @@
-const release = require('./package.json').version
-let Raven
+module.exports = function createHandler (options) {
+  const release = require('./package.json').version
+  let Raven
 
-if (process.env.RAVEN_DSN) {
-  Raven = require('raven')
-  Raven.config(process.env.RAVEN_DSN, {release}).install()
-}
-
-const request = require('superagent')
-const url = require('url')
-const { send } = require('micro')
-
-const parsers = require('./parsers')
-const transform = require('./transform')
-const { EmptyHttpResponseError, EmptyParseOutputError, InvalidInputError, NotFoundError } = require('./errors')
-
-const exampleLink = (host) => `http://${host}/?feed=https://rolflekang.com/feed.xml`
-
-async function parse (index, text) {
-  const parsed = await parsers[index](text)
-  if (!parsed) throw new EmptyParseOutputError()
-  return transform(parsed)
-}
-
-function handleError (res, error, extra) {
-  if (process.env.RAVEN_DSN) {
-    Raven.captureException(error, {
-      extra
-    })
+  if (options.ravenDsn) {
+    Raven = require('raven')
+    Raven.config(process.env.RAVEN_DSN, {release}).install()
   }
-  send(res, error.statusCode || 500, { error: error.constructor.name })
-  console.error(error)
-}
 
-module.exports = async (req, res) => {
-  const { query } = url.parse(req.url, true)
-  try {
-    if (!query.feed) {
-      return { usage: exampleLink(req.headers.host) }
+  const request = require('superagent')
+  const url = require('url')
+  const { send } = require('micro')
+
+  const parsers = require('./parsers')
+  const transform = require('./transform')
+  const { EmptyHttpResponseError, EmptyParseOutputError, InvalidInputError, NotFoundError } = require('./errors')
+
+  const exampleLink = (host) => `http://${host}/?feed=https://rolflekang.com/feed.xml`
+
+  async function parse (index, text) {
+    const parsed = await parsers[index](text)
+    if (!parsed) throw new EmptyParseOutputError()
+    return transform(parsed)
+  }
+
+  function handleError (res, error, extra) {
+    if (process.env.RAVEN_DSN) {
+      Raven.captureException(error, {
+        extra
+      })
     }
+    send(res, error.statusCode || 500, { error: error.constructor.name })
+    console.error(error)
+  }
 
-    if (typeof query.feed !== 'string') {
-      throw new InvalidInputError()
-    }
-
-    const response = await request(query.feed).buffer()
-    if (!response.text) throw new EmptyHttpResponseError()
-    if (response.notFound) throw new NotFoundError(query.feed)
-
-    let parsed
+  return async (req, res) => {
+    const { query } = url.parse(req.url, true)
     try {
-      parsed = await parse(0, response.text)
-    } catch (error) {
-      parsed = await parse(1, response.text)
-    }
+      if (!query.feed) {
+        return { usage: exampleLink(req.headers.host) }
+      }
 
-    return parsed
-  } catch (error) {
-    handleError(res, error, {query, url: req.url})
+      if (typeof query.feed !== 'string') {
+        throw new InvalidInputError()
+      }
+
+      const response = await request(query.feed).buffer()
+      if (!response.text) throw new EmptyHttpResponseError()
+      if (response.notFound) throw new NotFoundError(query.feed)
+
+      let parsed
+      try {
+        parsed = await parse(0, response.text)
+      } catch (error) {
+        parsed = await parse(1, response.text)
+      }
+
+      return parsed
+    } catch (error) {
+      handleError(res, error, {query, url: req.url})
+    }
   }
 }
