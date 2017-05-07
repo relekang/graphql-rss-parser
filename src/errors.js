937397a464bd02ae6b0f04394f8c9f721582eb46
Rolf Erik Lekang <me@rolflekang.com>
2017-05-07 20:32:37 +0200

    fix: Add raven reporting in graphql

--- src/errors.js
+++ src/errors.js
@@ -1,43 +1,81 @@
+const production = process.env.NODE_ENV === 'production'
+
 class BaseError extends Error {
-  constructor (message, statusCode) {
+  constructor(message, statusCode) {
     super(message)
     this.name = this.constructor.name
     this.statusCode = statusCode || 500
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor)
     } else {
-      this.stack = (new Error(message)).stack
+      this.stack = new Error(message).stack
     }
   }
 }
 
-class EmptyParseOutputError extends BaseError { }
+class EmptyParseOutputError extends BaseError {}
 
-class EmptyHttpResponseError extends BaseError { }
+class EmptyHttpResponseError extends BaseError {}
 
 class InvalidInputError extends BaseError {
-  constructor (message, statusCode) {
+  constructor(message, statusCode) {
     super(message, statusCode || 400)
   }
 }
 
 class NotFoundError extends BaseError {
-  constructor (url) {
+  constructor(url) {
     super('Could not find feed', 404)
     this.url = url
   }
 }
 
 class ParserError extends BaseError {
-  constructor (cause, statusCode) {
+  constructor(cause, statusCode) {
     super(cause.message, statusCode)
     this.cause = cause
   }
 }
 
 class NotAFeedError extends ParserError {
-  constructor (cause, statusCode) {
+  constructor(cause, statusCode) {
     super(cause, statusCode || 400)
+  }
+}
+
+function createErrorFormatter(Raven) {
+  return function formatError(error) {
+    if (Raven) {
+      if (error.path || error.name !== 'GraphQLError') {
+        Raven.captureException(
+          error,
+          raven.parsers.parseRequest(req, {
+            tags: { graphql: 'error' },
+            extra: {
+              source: error.source && error.source.body,
+              positions: error.positions,
+              path: error.path
+            }
+          })
+        )
+      } else {
+        Raven.captureMessage(
+          `GraphQLWrongQuery: ${error.message}`,
+          Raven.parsers.parseRequest(req, {
+            tags: { graphql: 'query' },
+            extra: {
+              source: error.source && error.source.body,
+              positions: error.positions
+            }
+          })
+        )
+      }
+    }
+
+    return {
+      name: error.message,
+      stack: production ? null : error.stack.split('\n')
+    }
   }
 }
 
@@ -47,5 +85,6 @@ module.exports = {
   InvalidInputError,
   ParserError,
   NotAFeedError,
-  NotFoundError
+  NotFoundError,
+  createErrorFormatter
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 const graphqlHTTP = require('express-graphql')
 
 const { Schema, root } = require('./schema')
+const { createErrorFormatter } = require('./errors')
 
-module.exports = function createHandler (options) {
+const production = process.env.NODE_ENV === 'production'
+
+module.exports = function createHandler(options) {
   let Raven
 
   if (options.ravenDsn) {
     Raven = require('raven')
-    Raven.config(process.env.RAVEN_DSN, {release: options.version}).install()
+    Raven.config(process.env.RAVEN_DSN, {
+      release: options.version,
+      environment: process.env.NODE_ENV
+    }).install()
   }
+
+  const formatError = createErrorFormatter(Raven)
 
   return graphqlHTTP({
     rootValue: root,
     schema: Schema,
-    pretty: true,
-    graphiql: true
-  });
+    pretty: !production,
+    graphiql: !production,
+		formatError,
+  })
 }
