117d789c0754b8f4cf11c5ff4562b15e3fbb9aef
Rolf Erik Lekang <me@rolflekang.com>
2017-05-10 21:56:48 +0200

    test: Mock requests in api tests

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -1,8 +1,32 @@
 /* eslint-env jest */
+const fs = require('fs-extra-promise')
 const micro = require('micro')
 const listen = require('test-listen')
 const request = require('superagent')
+const {resolve} = require('path')
+
 const createHandler = require('../')
+
+async function mockRequest(url) {
+  const path = resolve(__dirname + '/../../__fixtures__/' + url.replace(/https?:\/\//, '').replace(/\//g, '_'))
+  let content;
+  try {
+    content = (await fs.readFileAsync(path)).toString()
+  } catch(error) { }
+  if (!content) {
+    const response = await request(url).buffer()
+
+    if (!response.text) throw new EmptyHttpResponseError()
+    if (response.notFound) throw new NotFoundError(url)
+
+    content = response.text
+    await fs.writeFileAsync(path, content)
+  }
+
+  return content
+}
+
+jest.mock('../request', () => mockRequest)
 
 function testGraphqlApi (strings, ...args) {
   const query = String.raw(strings, ...args)
@@ -14,14 +38,19 @@ function testGraphqlApi (strings, ...args) {
     try {
       response = (await request
         .post(url)
+        .set('User-Agent', 'graphql-test')
         .set('Content-Type', 'application/json')
         .send(JSON.stringify({ query: 'query TestQuery { ' + query + ' }' }))
       ).body
     } catch (error) {
+      if (!error.response) {
+        throw error
+      }
+
       response = error.response.body
     }
     expect(response).toMatchSnapshot()
   })
 }
 
-module.exports = { testGraphqlApi }
+module.exports = { testGraphqlApi, mockRequest }
