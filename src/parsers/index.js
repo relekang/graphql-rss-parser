8044de70c2949c0e79f74d78f8bc8a0facdd3e02
Rolf Erik Lekang <me@rolflekang.com>
2017-12-10 22:00:43 +0100

    fix: Remove feedme from list of parsers
    Specifiying feedme will still work, however the it will not be tried in
    the loop of parsers.

--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  keys: ['FEEDPARSER', 'RSS_PARSER', 'FEEDME'],
+  keys: ['FEEDPARSER', 'RSS_PARSER'],
   RSS_PARSER: require('./rss-parser'),
   FEEDPARSER: require('./feedparser'),
   FEEDME: require('./feedme'),
