e65b1ee42bc246938f098135424e4df1efaf952b
Rolf Erik Lekang <me@rolflekang.com>
2018-02-23 22:56:39 +0100

    fix: Throw correct exception with axios

--- src/request.js
+++ src/request.js
@@ -3,20 +3,27 @@ const axios = require('axios')
 const { EmptyHttpResponseError, NotFoundError } = require('./errors')
 
 module.exports = async function request(url) {
-  const response = await axios({
-    url,
-    headers: {
-      'User-Agent': 'micro-rss-parser',
-    },
-  })
+  try {
+    const response = await axios({
+      url,
+      headers: {
+        'User-Agent': 'micro-rss-parser',
+      },
+    })
 
-  if (!response.data) throw new EmptyHttpResponseError()
-  if (response.status === 404) throw new NotFoundError(url)
+    if (!response.data) throw new EmptyHttpResponseError()
+    if (response.status === 404) throw new NotFoundError(url)
 
-  return {
-    text: response.data,
-    status: response.status,
-    contentType: response.headers['content-type'],
-    headers: response.headers,
+    return {
+      text: response.data,
+      status: response.status,
+      contentType: response.headers['content-type'],
+      headers: response.headers,
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new NotFoundError(url)
+    }
+    throw error
   }
 }
