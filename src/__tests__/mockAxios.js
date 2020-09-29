8a0f00b746bfee50c21cbf84a192ef3c4819af51
Rolf Erik Lekang <me@rolflekang.com>
2020-09-29 16:00:18 +0200

    refactor: Rewrite request mocking to mock axios
    This will ensure our request module is also tested.

---
+++ src/__tests__/mockAxios.js
@@ -0,0 +1,35 @@
+/* eslint-env jest */
+const fs = require('fs-extra-promise')
+const { resolve } = require('path')
+const axios = require('axios')
+
+async function mockAxios(options) {
+  const path = resolve(
+    __dirname,
+    '../../__fixtures__',
+    (options.url || '').replace(/https?:\/\//, '').replace(/\//g, '_')
+  )
+  let content
+  try {
+    content = JSON.parse((await fs.readFileAsync(path)).toString())
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    if (process.env.DEBUG_MOCKS) console.log(error)
+  }
+  if (!content) {
+    const response = await axios(options)
+
+    content = {
+      data: response.data,
+      status: response.status,
+      headers: response.headers || { ['content-type']: response['contentType'] },
+    }
+    await fs.writeFileAsync(path, JSON.stringify(content, null, 2))
+  }
+
+  return content
+}
+
+jest.mock('../axios', () => mockAxios)
+
+module.exports = mockAxios
