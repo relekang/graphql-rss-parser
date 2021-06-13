7ed710b4c9613fc2569c251d5a1d9df038a67def
Rolf Erik Lekang <me@rolflekang.com>
2021-06-13 22:19:05 +0200

    fix: Add types for jsonfeed and handle v1.1 spec

--- src/parsers/jsonfeed-v1.ts
+++ src/parsers/jsonfeed-v1.ts
@@ -2,23 +2,40 @@ import _debug from 'debug'
 import { ParserKey, ParserResponse } from '../types'
 const debug = _debug('graphql-rss-parser:parsers:json-parser')
 
-export async function parse(content: any): Promise<ParserResponse> {
+export async function parse(input: string): Promise<ParserResponse> {
+  const content: JsonFeed.Feed = JSON.parse(input)
   debug('starting to transform')
+
+  let author
+  if (content.version === 'https://jsonfeed.org/version/1') {
+    author = content.author?.name
+  } else {
+    author = content.authors?.map(({ name }) => name).find((n) => !!n)
+  }
+
   const output: ParserResponse = {
     parser: 'JSON_FEED_V1' as ParserKey,
     title: content.title,
     feedLink: content.feed_url,
     link: content.home_page_url,
-    author: content.author.name,
+    author,
     description: content.description,
-    entries: content.items.map((item: any) => {
-      item.guid = item.id
-      item.link = item.url
-      item.pubDate = item.date_published || item.date_modified
-      item.categories = item.tags
-      item.author = item.author.name
-      return item
-    }),
+    entries:
+      content.items?.map((item) => {
+        let author
+        if (content.version === 'https://jsonfeed.org/version/1') {
+          author = (item as JsonFeedV1.Item).author?.name
+        } else {
+          author = (item as JsonFeedV1_1.Item).authors?.map(({ name }) => name).find((n) => !!n)
+        }
+        return {
+          guid: item.id,
+          link: item.url,
+          pubDate: item.date_published || item.date_modified,
+          categories: item.tags,
+          author,
+        }
+      }) || [],
   }
   debug('done transform')
   return output
